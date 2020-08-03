275f157a8338b1c0de0ed24b16d7b35673969db7
millotp <pierre.millot@grenoble-inp.org>
2020-08-03 16:30:10 +0200

    boards: Added custom thingsat board based on nucleo-l053r8

---
+++ boards/thingsat/include/board.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2016-2017 Freie Universität Berlin
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @defgroup    boards_common_nucleo64 STM32 Nucleo-64
+ * @ingroup     boards
+ * @brief       Support for STM32 Nucleo-64 boards
+ * @{
+ *
+ * @file
+ * @brief       Common pin definitions and board configuration options
+ *
+ * @author      Hauke Petersen <hauke.petersen@fu-berlin.de>
+ * @author      Sebastian Meiling <s@mlng.net>
+ */
+
+#ifndef BOARD_H
+#define BOARD_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief   Initialize board specific hardware, including clock, LEDs and std-IO
+ */
+void board_init(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* BOARD_H */
+/** @} */
